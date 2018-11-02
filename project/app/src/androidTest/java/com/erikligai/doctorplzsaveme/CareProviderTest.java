554db87291b5d71bf4a9ed5f00038f8495499ff7
Erik Ligai <erik@Eriks-MBP.hitronhub.home>
2018-11-02 16:02:43 -0600

    tests

--- project/app/src/androidTest/java/com/erikligai/doctorplzsaveme/CareProviderTest.java
+++ project/app/src/androidTest/java/com/erikligai/doctorplzsaveme/CareProviderTest.java
@@ -4,7 +4,7 @@ import junit.framework.TestCase;
 
 public class CareProviderTest extends TestCase {
     public void testAddPatient(){
-        CareProvider careProvider = new CareProvider();
+        CareProvider careProvider = new CareProvider("name");
         Patient patient = new Patient("name");
         careProvider.addPatient(patient);
         assertTrue(careProvider.hasPatient(patient));
