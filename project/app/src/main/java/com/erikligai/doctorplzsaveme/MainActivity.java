9b6d6eb815dd66206820504bf269c9a7dd10a257
Daniil Tiganov <tiganov@ualberta.ca>
2018-11-11 13:00:34 -0600

    added new profile activity and a bit of functionality

--- project/app/src/main/java/com/erikligai/doctorplzsaveme/MainActivity.java
+++ project/app/src/main/java/com/erikligai/doctorplzsaveme/MainActivity.java
@@ -1,5 +1,6 @@
 package com.erikligai.doctorplzsaveme;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
@@ -7,20 +8,30 @@ import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
-    private Button patientButton = findViewById(R.id.patient_button);
-    private Button careProviderButton = findViewById(R.id.care_provider_button);
+    private Button patientButton;
+    private Button careProviderButton;
+
+
+    // TODO: ADD BACK (STACK) NAVIGATION
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        patientButton = (Button) findViewById(R.id.patient_button);
+        careProviderButton = (Button) findViewById(R.id.care_provider_button);
+
+
         // button listener for patientButton
         patientButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                // check if we need to make a new profile or not, and either launch the
-                // newProfileActivity or the displayProblems activity
+                // TODO: check if we need to make a new profile or not, and either launch the
+                // NewProfileActivity or the DisplayProblems activity
+
+                // for now, just do NewProfileActivity, for testing
+                startActivity(new Intent(MainActivity.this, NewProfileActivity.class));
             }
         });
 
@@ -28,6 +39,7 @@ public class MainActivity extends AppCompatActivity {
         careProviderButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
+                // TODO
 
             }
         });
