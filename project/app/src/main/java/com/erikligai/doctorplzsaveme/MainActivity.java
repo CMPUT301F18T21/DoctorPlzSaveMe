862f6d0481ebccf10d5d27e7931ccdda97d45746
Daniil Tiganov <tiganov@ualberta.ca>
2018-11-11 12:20:25 -0600

    added main screen ui and button listeners

--- project/app/src/main/java/com/erikligai/doctorplzsaveme/MainActivity.java
+++ project/app/src/main/java/com/erikligai/doctorplzsaveme/MainActivity.java
@@ -2,12 +2,36 @@ package com.erikligai.doctorplzsaveme;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+
+    private Button patientButton = findViewById(R.id.patient_button);
+    private Button careProviderButton = findViewById(R.id.care_provider_button);
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        // button listener for patientButton
+        patientButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // check if we need to make a new profile or not, and either launch the
+                // newProfileActivity or the displayProblems activity
+            }
+        });
+
+        // button listener for careProviderButton
+        careProviderButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+            }
+        });
+
     }
+
 }
