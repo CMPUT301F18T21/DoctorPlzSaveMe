a72f8c9557259e374fc246dc1e26edaf1b3c43c4
Kean Weng Yap <keanweng@ualberta.ca>
2018-11-13 15:44:39 -0700

    Added dummy main activity and MainProblemActivity

--- project/app/src/main/java/com/erikligai/doctorplzsaveme/MainActivity.java
+++ project/app/src/main/java/com/erikligai/doctorplzsaveme/MainActivity.java
@@ -1,13 +1,43 @@
 package com.erikligai.doctorplzsaveme;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
+import android.view.View;
+import android.widget.Button;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements View.OnClickListener{
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Toolbar myToolbar = findViewById(R.id.my_toolbar);
+        setSupportActionBar(myToolbar);
+
+        Button one = findViewById(R.id.viewProblemsButton);
+        one.setOnClickListener(this); // calling onClick() method
+    }
+
+    @Override
+    public void onClick(View v) {
+        // default method for handling onClick Events..
+        switch (v.getId()) {
+
+            case R.id.viewProblemsButton:
+                viewProblems(findViewById(R.id.content));
+                break;
+
+            default:
+                break;
+        }
+    }
+
+    /** Called when the user taps the View Problems button */
+    public void viewProblems(View view) {
+        Intent intent = new Intent(this, MainProblemActivity.class);
+        startActivity(intent);
     }
 }
---
+++ project/app/src/main/java/com/erikligai/doctorplzsaveme/MainProblemActivity.java
@@ -0,0 +1,67 @@
+package com.erikligai.doctorplzsaveme;
+
+import android.content.Intent;
+import android.support.design.widget.FloatingActionButton;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
+import android.util.Log;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+
+public class MainProblemActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main_problem);
+
+        Toolbar myToolbar = (Toolbar) findViewById(R.id.my_toolbar);
+        setSupportActionBar(myToolbar);
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+        getSupportActionBar().setDisplayShowHomeEnabled(true);
+        getSupportActionBar().setTitle(R.string.problems);
+
+        FloatingActionButton fab = findViewById(R.id.fab);
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+               Log.d("fab", "add problems!");
+               //calls AddProblemsActivity
+            }
+        });
+
+        Intent intent = getIntent();
+
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_search_toolbar, menu);
+
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case android.R.id.home:
+                finish();
+                return true;
+
+            case R.id.action_search:
+                // User chose the "Settings" item, show the app settings UI...
+                Log.d("toolbar", "search function!");
+                //calls ProblemSearchActivity
+                return true;
+
+            default:
+                // If we got here, the user's action was not recognized.
+                // Invoke the superclass to handle it.
+                return super.onOptionsItemSelected(item);
+
+        }
+    }
+}
