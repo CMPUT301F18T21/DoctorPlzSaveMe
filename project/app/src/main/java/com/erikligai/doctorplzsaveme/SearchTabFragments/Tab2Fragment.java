b552cf3f4e73aaed21dff6ff5bc91b9b6d5f3936
Kean Weng Yap <keanweng@ualberta.ca>
2018-11-17 16:58:01 -0700

    Added SupportMapFragment(map) for ProblemSearchActivity

--- project/app/src/main/java/com/erikligai/doctorplzsaveme/SearchTabFragments/Tab2Fragment.java
+++ project/app/src/main/java/com/erikligai/doctorplzsaveme/SearchTabFragments/Tab2Fragment.java
@@ -7,18 +7,35 @@ import android.view.View;
 import android.view.ViewGroup;
 
 import com.erikligai.doctorplzsaveme.R;
+import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.MapView;
+import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.MarkerOptions;
 
-public class Tab2Fragment extends Fragment {
+public class Tab2Fragment extends Fragment implements OnMapReadyCallback {
 
-    MapView mapView;
-    GoogleMap map;
+    MapView mMapView;
+    GoogleMap mGoogleMap;
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         View v = inflater.inflate(R.layout.fragment_search_two, container, false);
 
+        SupportMapFragment mapFragment = (SupportMapFragment) getChildFragmentManager().findFragmentById(R.id.map_search);
+
+        if (mapFragment != null) {
+            mapFragment.getMapAsync(this);
+        }
+
         return v;
+    }
+
+    public void onMapReady(GoogleMap map) {
+        map.addMarker(new MarkerOptions()
+                .position(new LatLng(0, 0))
+                .title("Marker"));
     }
 }
