1a5e6f5633f987c821e451ebf532a96cdc761e10
Daniil Tiganov <tiganov@ualberta.ca>
2018-11-16 18:24:41 -0600

    added edit profile activity

---
+++ project/app/src/main/java/com/erikligai/doctorplzsaveme/EditProfileActivity.java
@@ -0,0 +1,53 @@
+package com.erikligai.doctorplzsaveme;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+public class EditProfileActivity extends AppCompatActivity {
+
+    private TextView UserIdText;
+    private TextView EmailText;
+    private TextView PhoneText;
+
+    private Button CancelButton;
+    private Button SaveChangesButton;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_edit_profile);
+
+        // TODO: GET THESE FROM DB
+        String User_Id = "placeholderID";
+        String Email = "placeholderEmail";
+        String Phone = "(999)999-9999";
+
+        UserIdText = findViewById(R.id.user_id_textview);
+        UserIdText.setText(getString(R.string.user_id) + User_Id); // TODO: fix this
+        EmailText = findViewById(R.id.emailText);
+        EmailText.setText(Email);
+        PhoneText = findViewById(R.id.phoneText);
+        PhoneText.setText(Phone);
+
+        CancelButton = findViewById(R.id.cancelButton);
+        SaveChangesButton = findViewById(R.id.saveChangesButton);
+
+        CancelButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // TODO: RETURN TO MAIN (PATIENT) MENU
+            }
+        });
+
+        SaveChangesButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // TODO: UPDATE DB
+            }
+        });
+
+    }
+}
--- project/app/src/main/java/com/erikligai/doctorplzsaveme/NewProfileActivity.java
+++ project/app/src/main/java/com/erikligai/doctorplzsaveme/NewProfileActivity.java
@@ -1,13 +1,11 @@
 package com.erikligai.doctorplzsaveme;
 
+import android.content.Intent;
 import android.os.Bundle;
-import android.provider.ContactsContract;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
-
-import org.w3c.dom.Text;
 
 public class NewProfileActivity extends AppCompatActivity {
 
@@ -22,12 +20,12 @@ public class NewProfileActivity extends AppCompatActivity {
 
         createProfile = (Button) findViewById(R.id.createProfileButton);
         emailInputText = (TextView) findViewById(R.id.emailInputText);
-        phoneInputText = (TextView) findViewById(R.id.phoneInputText);
+        phoneInputText = (TextView) findViewById(R.id.phoneText);
 
         createProfile.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                // add profile to DB and launch ViewProblemsActivity
+                // TODO: add profile to DB and launch ViewProblemsActivity
             }
         });
 
