293c9b689823639ba4554d048ced3129bb1b53c3
Bruce Nie <ynie@ualberta.ca>
2018-11-17 18:22:11 -0700

    added RecordAdapter and cardview layout for recordlist activity

--- project/app/src/main/java/com/erikligai/doctorplzsaveme/RecordListActivity.java
+++ project/app/src/main/java/com/erikligai/doctorplzsaveme/RecordListActivity.java
@@ -6,17 +6,17 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
+import android.support.v7.widget.Toolbar;
 import android.util.Log;
 import android.view.View;
-import android.widget.Toolbar;
 
 import java.util.ArrayList;
 
 public class RecordListActivity extends AppCompatActivity {
-    private RecordAdapter adapter;
+    //private RecordAdapter adapter;
     private ArrayList<Record> records = new ArrayList<>();
 
-    //sample problem list
+    //sample record list
     Record r1 = new Record("Record1",null,"reordDescription");
     Record r2 = new Record("Record1",null,"reordDescription");
     Record r3 = new Record("Record1",null,"reordDescription");
@@ -29,36 +29,36 @@ public class RecordListActivity extends AppCompatActivity {
         records.add(r2);
         records.add(r3);
 
-        Toolbar myToolbar = (Toolbar) findViewById(R.id.my_toolbar);
+        Toolbar myToolbar = (Toolbar) findViewById(R.id.record_toolbar);
         setSupportActionBar(myToolbar);
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
         getSupportActionBar().setDisplayShowHomeEnabled(true);
         getSupportActionBar().setTitle(R.string.records);
 
-        FloatingActionButton fab = findViewById(R.id.fab);
+        FloatingActionButton fab = findViewById(R.id.record_fab);
         fab.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Log.d("fab", "add problems!");
-                //calls AddProblemsActivity
+                Log.d("fab", "add record");
+                //calls AddRecordActivity
             }
         });
 
-        RecyclerView problemRView = findViewById(R.id.problems_recyclerview);
-        problemRView.setHasFixedSize(true);
+        RecyclerView recordRecycler = findViewById(R.id.record_recyclerview);
+        recordRecycler.setHasFixedSize(true);
         RecyclerView.LayoutManager layoutManager = new LinearLayoutManager(this);
-        problemRView.setLayoutManager(layoutManager);
+        recordRecycler.setLayoutManager(layoutManager);
 
 
-        adapter = new ProblemAdapter(problems);
-        problemRView.setAdapter(adapter);
-        adapter.setOnEntryClickListener(new ProblemAdapter.OnEntryClickListener() {
-            @Override
-            public void onEntryClick(int position) {
-//                Intent intent = new Intent(getApplicationContext(), EditProblemActivity.class);
-//                intent.putExtra("Pos", position);
-//                startActivity(intent);
-            }
-        });
+//        adapter = new RecordAdapter(problems);
+//        recordRecycler.setAdapter(adapter);
+//        adapter.setOnEntryClickListener(new RecordAdapter.OnEntryClickListener() {
+//            @Override
+//            public void onEntryClick(int position) {
+////                Intent intent = new Intent(getApplicationContext(), EditProblemActivity.class);
+////                intent.putExtra("Pos", position);
+////                startActivity(intent);
+//            }
+//        });
     }
 }
