9e579153493b0afb3f1e514fe408310ad8516356
Erik <ligai@ualberta.ca>
2018-11-19 14:19:28 -0700

    tests

--- project/app/src/main/java/com/erikligai/doctorplzsaveme/CareProvider.java
+++ project/app/src/main/java/com/erikligai/doctorplzsaveme/CareProvider.java
@@ -25,7 +25,7 @@ public class CareProvider implements Profile {
     }
 
     public boolean hasPatient(Patient patient) {
-        return true;
+        return mPatientList.contains(patient);
     }
 }
 
