96a5c7b1b783207f5b810b20160da472a2991b99
Erik <ligai@ualberta.ca>
2018-11-20 12:47:16 -0700

    Added documentation for javadoc to CareProvider

--- project/app/src/main/java/com/erikligai/doctorplzsaveme/CareProvider.java
+++ project/app/src/main/java/com/erikligai/doctorplzsaveme/CareProvider.java
@@ -9,6 +9,13 @@ public class CareProvider implements Profile {
     String email;
     String phone;
 
+    /**
+     * Creates a CareProvider (extends Profile)
+     * @param name: Name of care provider
+     * @param id: user id of care provider
+     * @param email: User's email
+     * @param phone: User's phone number
+     */
     public CareProvider(String name, String id, String email, String phone) {
         this.name = name;
         this.id = id;
@@ -16,14 +23,27 @@ public class CareProvider implements Profile {
         this.phone = phone;
     }
 
+    /**
+     * Add patient to caregiver's list
+     * @param patient: Patient that is added
+     */
     public void addPatient(Patient patient) {
         mPatientList.add(patient);
     }
 
+    /**
+     * Gets the patient list of the care provider
+     * @return patient list of care provider
+     */
     public ArrayList getPatientList() {
         return mPatientList;
     }
 
+    /**
+     * Returns if careprovider has given patient or not
+     * @param patient: we want to check if patient exists in careprovider's list
+     * @return: boolean that returns true if patient is under careprovider, false if not
+     */
     public boolean hasPatient(Patient patient) {
         return mPatientList.contains(patient);
     }
