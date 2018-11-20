1abae73742d5ef3f298d3489c637154a226b4927
Kean Weng Yap <keanweng@ualberta.ca>
2018-11-19 23:31:38 -0700

    Espresso working

---
+++ project/app/src/androidTest/java/com/erikligai/doctorplzsaveme/HelloWorldEspressoTest.java
@@ -0,0 +1,29 @@
+package com.erikligai.doctorplzsaveme;
+
+
+import org.junit.Rule;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
+import androidx.test.filters.LargeTest;
+import androidx.test.rule.ActivityTestRule;
+import androidx.test.ext.junit.runners.AndroidJUnit4;
+
+import static androidx.test.espresso.Espresso.onView;
+import static androidx.test.espresso.assertion.ViewAssertions.matches;
+import static androidx.test.espresso.matcher.ViewMatchers.isDisplayed;
+import static androidx.test.espresso.matcher.ViewMatchers.withText;
+
+@RunWith(AndroidJUnit4.class)
+@LargeTest
+public class HelloWorldEspressoTest {
+
+    @Rule
+    public ActivityTestRule<MainActivity> mActivityRule =
+            new ActivityTestRule<>(MainActivity.class);
+
+    @Test
+    public void listGoesOverTheFold() {
+        onView(withText("Hello world!")).check(matches(isDisplayed()));
+    }
+}
