38a8a59e108f297545502425db5f95e6e6d5912d
Erik <ligai@ualberta.ca>
2018-11-30 18:00:52 -0700

    Finished qr login (display and scan)

--- project/app/src/main/java/com/erikligai/doctorplzsaveme/Activities/EditProfileActivity.java
+++ project/app/src/main/java/com/erikligai/doctorplzsaveme/Activities/EditProfileActivity.java
@@ -1,22 +1,28 @@
 package com.erikligai.doctorplzsaveme.Activities;
 
 import android.content.Intent;
-import android.support.v4.content.IntentCompat;
-import android.support.v7.app.AppCompatActivity;
+import android.graphics.Bitmap;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.Button;
+import android.widget.ImageView;
 import android.widget.TextView;
 import android.widget.Toast;
 
 import com.erikligai.doctorplzsaveme.R;
 import com.erikligai.doctorplzsaveme.StartAppActivities.MainActivity;
 import com.erikligai.doctorplzsaveme.backend.Backend;
+import com.google.zxing.BarcodeFormat;
+import com.google.zxing.MultiFormatWriter;
+import com.google.zxing.WriterException;
+import com.google.zxing.common.BitMatrix;
+import com.journeyapps.barcodescanner.BarcodeEncoder;
 
 public class EditProfileActivity extends AppCompatActivity {
 
     private TextView UserIdText, EmailText, PhoneText;
-    private Button CancelButton, SaveChangesButton, logOutButton;
+    private Button CancelButton, SaveChangesButton, logOutButton, displayQRButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -37,6 +43,18 @@ public class EditProfileActivity extends AppCompatActivity {
         CancelButton = findViewById(R.id.cancelButton);
         SaveChangesButton = findViewById(R.id.saveChangesButton);
         logOutButton = findViewById(R.id.logOutButton);
+//        displayQRButton = findViewById(R.id.displayQRButton);
+        ImageView qrCode = findViewById(R.id.qrImage);
+
+        MultiFormatWriter multiFormatWriter = new MultiFormatWriter();
+        try {
+            BitMatrix bitMatrix = multiFormatWriter.encode(User_Id, BarcodeFormat.QR_CODE, 220,220);
+            BarcodeEncoder barcodeEncoder = new BarcodeEncoder();
+            Bitmap bitmap = barcodeEncoder.createBitmap(bitMatrix);
+            qrCode.setImageBitmap(bitmap);
+        } catch (WriterException e) {
+            e.printStackTrace();
+        }
 
         CancelButton.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -70,6 +88,22 @@ public class EditProfileActivity extends AppCompatActivity {
                 }
             }
         });
+
+//        displayQRButton.setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View v) {
+//                MultiFormatWriter multiFormatWriter = new MultiFormatWriter();
+//                try {
+//                    BitMatrix bitMatrix = multiFormatWriter.encode(User_Id, BarcodeFormat.QR_CODE, 200,200);
+//                    BarcodeEncoder barcodeEncoder = new BarcodeEncoder();
+//                    Bitmap bitmap = barcodeEncoder.createBitmap(bitMatrix);
+//                    qrCode.setImageBitmap(bitmap);
+//                } catch (WriterException e) {
+//                    e.printStackTrace();
+//                }
+//
+//            }
+//        });
 
     }
 }
