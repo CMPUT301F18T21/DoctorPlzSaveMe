2dd9b35d45b9fdb37061d3d09f0fdc0e39590d71
heppelle <heppelle@ualberta.ca>
2018-12-02 16:34:44 -0700

    slideshow for patient added

--- project/app/src/main/java/com/erikligai/doctorplzsaveme/Activities/PatientSlideshowActivity.java
+++ project/app/src/main/java/com/erikligai/doctorplzsaveme/Activities/PatientSlideshowActivity.java
@@ -1,8 +1,13 @@
 package com.erikligai.doctorplzsaveme.Activities;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
 
+import com.erikligai.doctorplzsaveme.Adapters.RecyclerSlideshowAdapter;
+import com.erikligai.doctorplzsaveme.Adapters.RecyclerViewPhotoAdapter;
 import com.erikligai.doctorplzsaveme.R;
 
 public class PatientSlideshowActivity extends AppCompatActivity {
@@ -12,5 +17,12 @@ public class PatientSlideshowActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_patient_slideshow);
 
+        Intent intent = getIntent();
+
+        RecyclerView photoRecycler = findViewById(R.id.PhotoRecyclerView);
+        photoRecycler.setHasFixedSize(true);
+        RecyclerSlideshowAdapter adapter = new RecyclerSlideshowAdapter(this,intent.getIntExtra("Pos",-1));
+        photoRecycler.setAdapter(adapter);
+        photoRecycler.setLayoutManager(new LinearLayoutManager(this, LinearLayoutManager.VERTICAL, false));
     }
 }
