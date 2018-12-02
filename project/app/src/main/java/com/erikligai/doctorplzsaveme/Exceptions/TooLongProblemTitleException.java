5f9a0c8afd1e0b4853a29259cbaa49b0675af4e5
Daniil Tiganov <tiganov@ualberta.ca>
2018-12-02 01:47:21 -0700

    added javadoc for exceptions

--- project/app/src/main/java/com/erikligai/doctorplzsaveme/Exceptions/TooLongProblemTitleException.java
+++ project/app/src/main/java/com/erikligai/doctorplzsaveme/Exceptions/TooLongProblemTitleException.java
@@ -1,5 +1,8 @@
 package com.erikligai.doctorplzsaveme.Exceptions;
 
+/**
+ * class for too long of problem title exception (so we can catch it specifically)
+ */
 public class TooLongProblemTitleException extends Exception {
     public TooLongProblemTitleException(String message){
         super(message);
