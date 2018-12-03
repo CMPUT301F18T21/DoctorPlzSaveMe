b396c30a5e005168df61a799c565e9c3ca79c381
Kean Weng Yap <keanweng@ualberta.ca>
2018-12-02 18:05:10 -0700

    View record reuse fragment

--- project/app/src/main/java/com/erikligai/doctorplzsaveme/Activities/EditRecordThreeActivity.java
+++ project/app/src/main/java/com/erikligai/doctorplzsaveme/Activities/EditRecordThreeActivity.java
@@ -1,9 +1,11 @@
 package com.erikligai.doctorplzsaveme.Activities;
 
+import android.app.Activity;
 import android.content.Intent;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
 import android.graphics.Matrix;
+import android.provider.MediaStore;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Base64;
@@ -11,11 +13,13 @@ import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.ImageView;
+import android.widget.Toast;
 
 import com.erikligai.doctorplzsaveme.Models.Record;
 import com.erikligai.doctorplzsaveme.Models.RecordBuffer;
 import com.erikligai.doctorplzsaveme.R;
 
+import java.io.ByteArrayOutputStream;
 import java.util.ArrayList;
 
 public class EditRecordThreeActivity extends AppCompatActivity implements View.OnClickListener{
@@ -94,6 +98,15 @@ public class EditRecordThreeActivity extends AppCompatActivity implements View.O
                 zoomImage(10);
                 break;
 
+            case R.id.imageView12:
+                Log.d("size", Integer.toString(photos.size()));
+                if (photos.size()<11) {
+                    dispatchTakePictureIntent();
+                } else {
+                    Toast.makeText(getApplicationContext(), "Only a maximum of 11 photos per Record!", Toast.LENGTH_SHORT).show();
+                }
+                break;
+
             case R.id.imageView13:
                 zoomImg.setVisibility(View.GONE);
                 break;
@@ -105,6 +118,43 @@ public class EditRecordThreeActivity extends AppCompatActivity implements View.O
         super.onResume();
         findViewById(R.id.imageView12).setVisibility(View.INVISIBLE);
         displayPhotos();
+    }
+
+    static final int ACTION_IMAGE_CAPTURE = 1;
+    //static final int REQUEST_IMAGE_CAPTURE = 1;
+
+    private void dispatchTakePictureIntent() {
+        // finish current display before calling camera.
+        Intent takePictureIntent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+        startActivityForResult(takePictureIntent, ACTION_IMAGE_CAPTURE);
+        Log.i("message", "String");
+    }
+
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+
+        Bitmap photo;
+        if (requestCode == ACTION_IMAGE_CAPTURE && resultCode == Activity.RESULT_OK) {
+            photo = (Bitmap) data.getExtras().get("data");
+            String photoStr = getStringFromBitmap(photo);
+            photos.add(photoStr);
+            Log.i("photo", photoStr);
+            Toast.makeText(getApplicationContext(), "Photo added!", Toast.LENGTH_SHORT).show();
+        }
+    }
+
+    private String getStringFromBitmap(Bitmap bitmapPicture) {
+        /*
+         * This functions converts Bitmap picture to a string which can be
+         * JSONified.
+         * */
+        final int COMPRESSION_QUALITY = 100;
+        String encodedImage;
+        ByteArrayOutputStream byteArrayBitmapStream = new ByteArrayOutputStream();
+        bitmapPicture.compress(Bitmap.CompressFormat.PNG, COMPRESSION_QUALITY,
+                byteArrayBitmapStream);
+        byte[] b = byteArrayBitmapStream.toByteArray();
+        encodedImage = Base64.encodeToString(b, Base64.DEFAULT);
+        return encodedImage;
     }
 
     public Bitmap StringToBitMap(String encodedString) {
