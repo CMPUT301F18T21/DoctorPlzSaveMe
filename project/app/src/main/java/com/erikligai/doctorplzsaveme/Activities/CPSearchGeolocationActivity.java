c558fe53f407c5b9cd05460a4e46f573fddc5da6
Erik <ligai@ualberta.ca>
2018-12-03 04:17:08 -0700

    Search for keyword and body location works for cp

---
+++ project/app/src/main/java/com/erikligai/doctorplzsaveme/Activities/CPSearchGeolocationActivity.java
@@ -0,0 +1,15 @@
+package com.erikligai.doctorplzsaveme.Activities;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+import com.erikligai.doctorplzsaveme.R;
+
+public class CPSearchGeolocationActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_cpsearch_geolocation);
+    }
+}
