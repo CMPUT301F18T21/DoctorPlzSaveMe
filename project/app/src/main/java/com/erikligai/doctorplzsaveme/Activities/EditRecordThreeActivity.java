1ff345386b5f6d29ea76a6a737640e098a43a089
heppelle <heppelle@ualberta.ca>
2018-12-03 22:28:36 -0700

    fiexd permissions on camera

--- project/app/src/main/java/com/erikligai/doctorplzsaveme/Activities/EditRecordThreeActivity.java
+++ project/app/src/main/java/com/erikligai/doctorplzsaveme/Activities/EditRecordThreeActivity.java
@@ -1,10 +1,14 @@
 package com.erikligai.doctorplzsaveme.Activities;
 
+import android.Manifest;
 import android.app.Activity;
 import android.content.Intent;
+import android.content.pm.PackageManager;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
 import android.provider.MediaStore;
+import android.support.v4.app.ActivityCompat;
+import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Base64;
@@ -177,12 +181,46 @@ public class EditRecordThreeActivity extends AppCompatActivity implements View.O
     }
 
     static final int ACTION_IMAGE_CAPTURE = 1;
+    static final int MY_PERMISSIONS_REQUEST_CAMERA = 100;
 
     private void dispatchTakePictureIntent() {
+
         // finish current display before calling camera.
-        Intent takePictureIntent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
-        startActivityForResult(takePictureIntent, ACTION_IMAGE_CAPTURE);
-        Log.i("message", "String");
+        // Here, thisActivity is the current activity
+        if (ContextCompat.checkSelfPermission(this,
+                Manifest.permission.CAMERA)
+                != PackageManager.PERMISSION_GRANTED) {
+            ActivityCompat.requestPermissions(this,
+                    new String[]{Manifest.permission.CAMERA}, MY_PERMISSIONS_REQUEST_CAMERA);
+        }
+        else{
+            Intent takePictureIntent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+            startActivityForResult(takePictureIntent, ACTION_IMAGE_CAPTURE);
+        }
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode,
+                                           String permissions[], int[] grantResults) {
+        switch (requestCode) {
+            case MY_PERMISSIONS_REQUEST_CAMERA: {
+                // If request is cancelled, the result arrays are empty.
+                if (grantResults.length > 0
+                        && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                    Intent takePictureIntent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+                    startActivityForResult(takePictureIntent, ACTION_IMAGE_CAPTURE);
+                    // permission was granted, yay! Do the
+                    // contacts-related task you need to do.
+                } else {
+                    Toast.makeText(getApplicationContext(), "Camera denied", Toast.LENGTH_SHORT).show();
+                    // permission denied, boo! Disable the
+                    // functionality that depends on this permission.
+                }
+                return;
+            }
+            // other 'case' lines to check for other
+            // permissions this app might request.
+        }
     }
 
     protected void onActivityResult(int requestCode, int resultCode, Intent data) {
--- project/app/src/main/java/com/erikligai/doctorplzsaveme/Activities/UploadBodyLocationActivity.java
+++ project/app/src/main/java/com/erikligai/doctorplzsaveme/Activities/UploadBodyLocationActivity.java
@@ -1,9 +1,13 @@
 package com.erikligai.doctorplzsaveme.Activities;
 
+import android.Manifest;
 import android.app.Activity;
 import android.content.Intent;
+import android.content.pm.PackageManager;
 import android.graphics.Bitmap;
 import android.provider.MediaStore;
+import android.support.v4.app.ActivityCompat;
+import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.LinearLayoutManager;
@@ -21,6 +25,7 @@ import com.erikligai.doctorplzsaveme.backend.Backend;
 import java.io.ByteArrayOutputStream;
 
 public class UploadBodyLocationActivity extends AppCompatActivity implements View.OnClickListener {
+    private RecyclerViewPhotoAdapter adapter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -29,11 +34,18 @@ public class UploadBodyLocationActivity extends AppCompatActivity implements Vie
 
         RecyclerView photoRecycler = findViewById(R.id.PhotoRecyclerView);
         photoRecycler.setHasFixedSize(true);
-        RecyclerViewPhotoAdapter adapter = new RecyclerViewPhotoAdapter(this);
+        adapter = new RecyclerViewPhotoAdapter(this);
         photoRecycler.setAdapter(adapter);
         photoRecycler.setLayoutManager(new LinearLayoutManager(this, LinearLayoutManager.HORIZONTAL, false));
         Button uploadNewPhoto = findViewById(R.id.addPhotoButton);
         uploadNewPhoto.setOnClickListener(this);
+    }
+
+    protected void onResume() {
+        // TODO Auto-generated method stub
+        super.onResume();
+        //findViewById(R.id.imageView12).setVisibility(View.INVISIBLE);
+        adapter.notifyDataSetChanged();
     }
 
     @Override
@@ -47,14 +59,51 @@ public class UploadBodyLocationActivity extends AppCompatActivity implements Vie
         }
     }
 
+
     static final int ACTION_IMAGE_CAPTURE = 1;
-    //static final int REQUEST_IMAGE_CAPTURE = 1;
+    static final int MY_PERMISSIONS_REQUEST_CAMERA = 100;
 
     private void dispatchTakePictureIntent() {
+
         // finish current display before calling camera.
-        Intent takePictureIntent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
-        startActivityForResult(takePictureIntent, ACTION_IMAGE_CAPTURE);
-        Log.i("message", "String");
+        // Here, thisActivity is the current activity
+        if (ContextCompat.checkSelfPermission(this,
+                Manifest.permission.CAMERA)
+                != PackageManager.PERMISSION_GRANTED) {
+            ActivityCompat.requestPermissions(this,
+                    new String[]{Manifest.permission.CAMERA}, MY_PERMISSIONS_REQUEST_CAMERA);
+        }
+        else{
+            Log.i("CAMERA OPENED", "TWO");
+            Intent takePictureIntent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+            startActivityForResult(takePictureIntent, ACTION_IMAGE_CAPTURE);
+        }
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode,
+                                           String permissions[], int[] grantResults) {
+        switch (requestCode) {
+            case MY_PERMISSIONS_REQUEST_CAMERA: {
+                // If request is cancelled, the result arrays are empty.
+                if (grantResults.length > 0
+                        && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                    Log.i("CAMERA OPENED", "TWO");
+                    Intent takePictureIntent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+                    startActivityForResult(takePictureIntent, ACTION_IMAGE_CAPTURE);
+
+                    // permission was granted, yay! Do the
+                    // contacts-related task you need to do.
+                } else {
+                    Toast.makeText(getApplicationContext(), "Camera denied", Toast.LENGTH_SHORT).show();
+                    // permission denied, boo! Disable the
+                    // functionality that depends on this permission.
+                }
+            }
+
+            // other 'case' lines to check for other
+            // permissions this app might request.
+        }
     }
 
     protected void onActivityResult(int requestCode, int resultCode, Intent data) {
@@ -62,7 +111,7 @@ public class UploadBodyLocationActivity extends AppCompatActivity implements Vie
         Bitmap photo;
         String photoLabel = "";
 
-        Intent intent = new Intent(this, UploadBodyLocationActivity.class);
+        //Intent intent = new Intent(this, UploadBodyLocationActivity.class);
         if (requestCode == ACTION_IMAGE_CAPTURE && resultCode == Activity.RESULT_OK) {
             photo = (Bitmap) data.getExtras().get("data");
             String photoStr = getStringFromBitmap(photo);
@@ -71,12 +120,8 @@ public class UploadBodyLocationActivity extends AppCompatActivity implements Vie
             Log.i("photoId", photoId);
             Backend.getInstance().getPatientProfile().addPhoto(photoId, photoStr, photoLabel);                           // store photo in backend.
             Toast.makeText(getApplicationContext(), "Photo added!", Toast.LENGTH_SHORT).show();
+            //Backend.getInstance().UpdatePatient();
         }
-
-        Backend.getInstance().UpdatePatient();
-        //intent.putExtra("photoList", photoList);
-        finish();
-        startActivity(intent);
     }
 
     private String getStringFromBitmap(Bitmap bitmapPicture) {
@@ -92,5 +137,11 @@ public class UploadBodyLocationActivity extends AppCompatActivity implements Vie
         byte[] b = byteArrayBitmapStream.toByteArray();
         encodedImage = Base64.encodeToString(b, Base64.DEFAULT);
         return encodedImage;
+    }
+
+    @Override
+    public void onBackPressed() {
+        Backend.getInstance().UpdatePatient();
+        finish();
     }
 }
